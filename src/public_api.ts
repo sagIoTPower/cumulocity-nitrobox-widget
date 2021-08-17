b6b11cbf010b75180e3733a22cb2c3e6835535f0
Christof Strack (ck) <ck@SAG-GWQHSQ2.localdomain>
2021-08-17 10:16:58 +0200

    changed products structure to array

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,4 +1,4 @@
 /* Add your widget's module(s) here */
 
 export { DemoWidgetModule } from "./demo-widget/demo-widget.module";
-export { GpDeviceDetailsWidgetModule } from "./gp-device-details-widget/gp-device-details-widget.module";
+export { NxDeviceDetailsWidgetModule } from "./nx-device-details-widget/nx-device-details-widget.module";
